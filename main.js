0ddee2a8b3ef90ff06d3073d224dba04e79b1c24
JPantana <joshpantana@me.com>
2019-03-25 20:28:30 -0500

    ingredient form is working

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('MOOOOOO');
+const addButton = document.getElementById('addButton');
+const inputIngredient = document.getElementById('ingredientInput');
+
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const addIngredient = (e) => {
+    // e.preventDefault();
+    let inputText = inputIngredient.value;
+    console.log(inputText);
+    printToDom('ingredient-container', inputText);
+    inputIngredient.value = '';
+};
+
+const buttonEvents = () => {
+    addButton.addEventListener('click', addIngredient);
+};
+
+const init = () => {
+    buttonEvents();
+};
+init();
