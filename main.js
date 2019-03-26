e7155dd9a5e18796ec5f136753502444b72c40c7
JPantana <joshpantana@me.com>
2019-03-25 20:57:00 -0500

    cards are printing

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 const addButton = document.getElementById('addButton');
 const inputIngredient = document.getElementById('ingredientInput');
 
+const ingredients = [];
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += textToPrint;
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const domStringBuilder = (arrayToPrint) => {
+    console.log(arrayToPrint);
+    let domString = '';
+    arrayToPrint.forEach((ingredient) => {
+        domString += `<div class="card col-3">`;
+        domString += `  <div class="card-body">`;
+        domString += `      <h5 class="card-title">${ingredient.item}</h5>`;
+        // domString += `      <a href="#" class="btn btn-primary">Go somewhere</a>`;
+        domString += `  </div>`;
+        domString += `</div>`;
+    });
+    
+    printToDom('ingredient-container', domString);
+
 };
 
 const addIngredient = (e) => {
     // e.preventDefault();
     let inputText = inputIngredient.value;
     console.log(inputText);
-    printToDom('ingredient-container', inputText);
+    let newArray = {
+        item: inputText,
+    }
+    ingredients.push(newArray);
+    domStringBuilder(ingredients);
     inputIngredient.value = '';
 };
 
